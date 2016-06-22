88e6f7bbf6c098a4ab88c8a64a3236b8955e4687
Dave Cohen <dave@project768.com>
2016-06-22 14:45:34 +0200

    add python and ansible versions of playbook skeleton creation

---
+++ 02-playbooks/01-playbook-structure/create_playbook.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+"""
+Usage: ./create_playbook.py /path/to/playbookname [role1 role2 ...]
+Creates an empty playbook skeleton, with any roles that are specified.
+
+e.g. ./create_playbook.py /tmp/pyplaybook web db cache
+
+
+If /path/to/playbookname already exists, we only create the roles that don't exist yet, according to our structure.
+
+
+Our structure is loosely based on https://github.com/enginyoyen/ansible-best-practises:
+
+production.ini            # inventory file for production stage
+development.ini           # inventory file for development stage
+test.ini                  # inventory file for test stage
+vpass                     # ansible-vault password file
+                          # This file should not be committed into the repository
+                          # therefore file is in ignored by git
+group_vars/
+    all/                  # variables under this directory belongs all the groups
+        apt.yml           # ansible-apt role variable file for all groups
+    webservers/           # here we assign variables to webservers groups
+        apt.yml           # Each file will correspond to a role i.e. apt.yml
+        nginx.yml         # ""
+    postgresql/           # here we assign variables to postgresql groups
+        postgresql.yml    # Each file will correspond to a role i.e. postgresql
+        postgresql-password.yml   # Encrypted password file
+plays/
+    ansible.cfg           # Ansible.cfg file that holds all ansible config
+    webservers.yml        # playbook for webserver tier
+    postgresql.yml        # playbook for postgresql tier
+
+roles/
+    roles_requirements.yml# All the information about the roles
+    external              # All the roles that are in git or ansible galaxy
+                          # Roles that are in roles_requirements.yml file will be downloaded into this directory
+    internal              # All the roles that are not public
+
+extension/
+    setup                 # All the setup files for updating roles and ansible dependencies
+
+"""
+
+import sys
+import os
+
+
+def main():
+    # Argument parsing
+    args = sys.argv
+    if len(args) < 2:
+        print("Invalid invocation. Exiting.")
+
+    playbook_location = args[1]
+    roles_to_create = args[2:]
+
+    # If that location already exists, fail
+    if os.path.isdir(playbook_location) or os.path.isfile(playbook_location):
+        print("Can't create a playbook with that path: something already exists there!")
+        return
+
+    # Looks like everything is OK, let's create the playbook.
+    print("Creating playbook skeleton")
+    create_playbook(playbook_location)
+
+    for r in roles_to_create:
+        print("Creating role: ", r)
+        create_role(playbook_location, r)
+
+
+def create_playbook(location):
+    """
+    Create an empty playbook skeleton.
+    """
+    dirs_to_create = [
+        "group_vars",
+        "plays",
+        "roles",
+    ]
+    files_to_create = [
+        "production.ini",
+        "development.ini",
+        "hosts.ini",
+        os.path.join("group_vars", "all"),
+        os.path.join("plays", "ansible.cfg"),
+    ]
+
+    # Create the playbook skeleton
+    print("Creating playbook directories")
+    os.mkdir(location)
+    for d in dirs_to_create:
+        os.mkdir(os.path.join(location, d))
+
+    print("Creating playbook files")
+    for f in files_to_create:
+        open(os.path.join(location, f), 'a').close()
+
+
+def create_role(location, rolename):
+    """
+    Add a role to the playbook at LOCATION (an absolute path)
+    """
+    dirs_to_create = [
+        os.path.join(location, "roles", rolename),
+        os.path.join(location, "roles", rolename, "tasks"),
+        os.path.join(location, "roles", rolename, "handlers"),
+        os.path.join(location, "roles", rolename, "templates"),
+    ]
+    files_to_create = [
+        os.path.join(location, "roles", rolename, "tasks", "main.yml"),
+        os.path.join(location, "roles", rolename, "handlers", "main.yml"),
+    ]
+
+    # Create the role skeleton
+    print("Creating role directories")
+    for d in dirs_to_create:
+        os.mkdir(os.path.join(location, d))
+
+    print("Creating role files")
+    for f in files_to_create:
+        open(os.path.join(location, f), 'a').close()
+
+
+# MAIN:
+main()
