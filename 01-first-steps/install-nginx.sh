d65d6939b3f0340492e96cb58139f84cff71049e
D Schwizzle <dave@example.com>
2016-09-18 16:25:56 +0200

    get these scripts into production-ready shape

---
+++ 01-first-steps/install-nginx.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+
+# Install nginx.
+apt-get update
+apt-get install -y nginx vim sshguard nano curl
+
+# Ensure website directory exists
+mkdir -p /var/www/mywebsite
+
+# Copy nginx config files (main config and vhost config)
+cp config/nginx.conf /etc/nginx/nginx.conf
+chown root:root /etc/nginx/nginx.conf
+chmod 0644 /etc/nginx/nginx.conf
+
+cp config/mywebsite.conf /etc/nginx/conf.d/mywebsite.conf
+chown root:root /etc/nginx/conf.d/mywebsite.conf
+chmod 0644 /etc/nginx/conf.d/mywebsite.conf
+
+# Copy our website files
+cp config/index.html /var/www/mywebsite/
+chown root:root /var/www/mywebsite/index.html
+chmod 0644 /var/www/mywebsite/index.html
+
+# Remove default nginx vhost configuration
+rm /etc/nginx/sites-enabled/default
+
+# Start nginx at boot + make sure it's running right now.
+service nginx restart
+service nginx enable
+
+# On a machine with systemd, you'd use these instead
+# systemctl enable nginx
+# systemctl restart nginx
+
+
+
+
